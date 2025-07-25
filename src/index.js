58057ab7eaef468666551e60d734ed794c1fe7c2
Liberian Post <info@liberianpost.com>
2025-07-25 19:41:00 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // Make sure this file exists: src/App.jsx or src/App.js
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
