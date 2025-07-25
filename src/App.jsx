29b3397f219281bd5ec2c65c9c9bd4e4826e1077
Liberian Post <info@liberianpost.com>
2025-07-25 19:30:25 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './components/Hero';
+import './index.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center">
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
