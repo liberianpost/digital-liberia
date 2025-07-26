3f8b6f657138562353ec95943f7c895bba7c29df
Liberian Post <info@liberianpost.com>
2025-07-26 11:04:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';  // Make sure this file exists: src/App.jsx or src/App.js
+import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+);
