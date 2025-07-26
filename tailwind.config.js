ec6018139d190146fd31501ed28d1289d4168213
Liberian Post <info@liberianpost.com>
2025-07-26 22:56:55 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        background: "gradient 10s ease infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+      },
+      backgroundImage: {
+        'gradient-blue': 'linear-gradient(270deg, #0ea5e9, #3b82f6, #60a5fa, #0ea5e9)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
