2396cc3b98e50ba3648f1a5fa761737774c13ea2
Liberian Post <info@liberianpost.com>
2025-07-26 13:01:10 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div style={{ padding: 50, fontFamily: "Arial" }}>
-      <h1>Welcome to Digital Liberia</h1>
-      <p>This site is under development.</p>
+    <div className="p-8 font-sans">
+      <Hero />
     </div>
   );
 }
