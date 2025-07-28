3f0a574bfce8fda0e1e1c94f8831ac47b929527c
Liberian Post <info@liberianpost.com>
2025-07-28 12:06:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import System from "./System";
 
 function App() {
   return (
-    <div className="p-8 font-sans">
-      <Home />
-    </div>
+    <Router>
+      <div className="p-8 font-sans">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/system" element={<System />} />
+          {/* Add other routes for your navigation links */}
+          <Route path="/digital-liberia" element={<div>Digital Liberia Page (Coming Soon)</div>} />
+          <Route path="/libpay" element={<div>LibPay Page (Coming Soon)</div>} />
+          <Route path="/liberian-post" element={<div>Liberian Post Page (Coming Soon)</div>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
