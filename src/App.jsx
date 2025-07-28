20a6658177b69b848a9ad5170815655b6cfd67c9
Liberian Post <info@liberianpost.com>
2025-07-28 17:29:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import System from "./System";
+import Dssn from "./Dssn";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="system" element={<System />} />
+        <Route path="dssn" element={<Dssn />} />
         <Route path="digital-liberia" element={<div>Coming Soon</div>} />
         <Route path="libpay" element={<div>Coming Soon</div>} />
         <Route path="liberian-post" element={<div>Coming Soon</div>} />
