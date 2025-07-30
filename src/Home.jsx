295ff7d765704fbbd1f641c8f0292998f3ce8132
Liberian Post <info@liberianpost.com>
2025-07-30 19:18:06 +0200

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -279,7 +279,7 @@ export default function Home() {
                       <svg className="w-5 h-5 mr-2 text-amber-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                       </svg>
-                      <a href="mailto:inf@liberianpost.com" className="hover:text-amber-300 transition-colors">inf@liberianpost.com</a>
+                      <a href="mailto:info@liberianpost.com" className="hover:text-amber-300 transition-colors">inf@liberianpost.com</a>
                     </li>
                   </ul>
                 </div>
