b4095da5f05719bcd1db90f97f6e99aee45a300d
Liberian Post <info@liberianpost.com>
2025-08-01 20:19:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import Home from "./Home";
 import System from "./System";
 import Dssn from "./Dssn";
 import Digitalliberia from "./Digitalliberia";
 import Libpay from "./Libpay";
+import MoeDashboard from "./System"; // Import your dashboard component
+import UnauthorizedPage from "./System"; // Import your unauthorized component
 
 function App() {
   return (
-    <div className="app-container">
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="system" element={<System />} />
-        <Route path="dssn" element={<Dssn />} />
-        <Route path="digital-liberia" element={<Digitalliberia />} />
-        <Route path="libpay" element={<Libpay />} />
-        <Route path="liberian-post" element={<div>Coming Soon</div>} />
-        <Route path="*" element={<Home />} /> {/* Fallback */}
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app-container">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="system" element={<System />} />
+          <Route path="dssn" element={<Dssn />} />
+          <Route path="digital-liberia" element={<Digitalliberia />} />
+          <Route path="libpay" element={<Libpay />} />
+          <Route path="moe-dashboard" element={<MoeDashboard />} />
+          <Route path="unauthorized" element={<UnauthorizedPage />} />
+          <Route path="liberian-post" element={<div>Coming Soon</div>} />
+          <Route path="*" element={<Home />} /> {/* Fallback */}
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
