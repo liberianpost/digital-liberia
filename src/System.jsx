c4af1d6c644aeb2ad2b14460d7eeb7bdbaf39026
Liberian Post <info@liberianpost.com>
2025-08-01 20:26:44 +0200

    Update System.jsx

--- src/System.jsx
+++ src/System.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext.js";
+import { useAuth } from "../context/AuthContext";
 import { SecurityLevels, getRoleName } from "../utils/auth.js";
 import { DashboardItems } from "../config/dashboardItems";
 
