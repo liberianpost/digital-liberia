7b524c6c6331dc891e7b60a70c02b8cee3d031f8
Liberian Post <info@liberianpost.com>
2025-08-15 20:35:05 +0200

    Create securityLevels.js

---
+++ src/utils/securityLevels.js
@@ -0,0 +1,13 @@
+console.log('securityLevels.js - Starting to load SecurityLevels');
+
+export const SecurityLevels = {
+  STUDENT: 'STUDENT',
+  PARENT: 'PARENT',
+  TEACHER: 'TEACHER',
+  SCHOOL_ADMIN: 'SCHOOL_ADMIN',
+  MINISTRY_OFFICIAL: 'MINISTRY_OFFICIAL',
+  DATABASE_ADMIN: 'DATABASE_ADMIN',
+  SYSTEM_ADMIN: 'SYSTEM_ADMIN',
+};
+
+console.log('securityLevels.js - SecurityLevels loaded successfully');
