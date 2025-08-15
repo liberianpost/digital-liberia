c7e2ab252542358e6e0cec66b72d2b265c351e20
Liberian Post <info@liberianpost.com>
2025-08-16 00:56:56 +0200

    Update AppRoutes.jsx

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -12,8 +12,19 @@ import StudentRecords from '@components/StudentRecords';
 import SchoolManagement from '@components/SchoolManagement';
 import TeacherManagement from '@components/TeacherManagement';
 import DistrictReports from '@components/DistrictReports';
+import DistrictOverview from '@components/DistrictOverview';
 import DatabaseTools from '@components/DatabaseTools';
 import UserManagement from '@components/UserManagement';
+import AddParent from '@components/AddParent';
+import AnnouncementManagement from '@components/AnnouncementManagement';
+import ComplianceReports from '@components/ComplianceReports';
+import MinistryEmployeeManagement from '@components/MinistryEmployeeManagement';
+import ParentDetails from '@components/ParentDetails';
+import ParentManagement from '@components/ParentManagement';
+import Reports from '@components/Reports';
+import SchoolAdminManagement from '@components/SchoolAdminManagement';
+import SchoolReports from '@components/SchoolReports';
+import TeacherReports from '@components/TeacherReports';
 
 const AppRoutes = () => {
   console.log('AppRoutes.jsx - Rendering AppRoutes component');
@@ -38,7 +49,7 @@ const AppRoutes = () => {
         }
       />
       <Route
-        path="/moe/student-profile"
+        path="/moe/student-profile/:studentId"
         element={
           <ProtectedRoute>
             {console.log('AppRoutes.jsx - Rendering StudentProfile component')}
@@ -83,6 +94,15 @@ const AppRoutes = () => {
         }
       />
       <Route
+        path="/moe/add-class"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering AddClass component')}
+            <div>AddClass component placeholder</div>
+          </ProtectedRoute>
+        }
+      />
+      <Route
         path="/moe/student-records"
         element={
           <ProtectedRoute>
@@ -110,6 +130,15 @@ const AppRoutes = () => {
         }
       />
       <Route
+        path="/moe/teacher-details/:teacherId"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering TeacherDetails component')}
+            <div>TeacherDetails component placeholder</div>
+          </ProtectedRoute>
+        }
+      />
+      <Route
         path="/moe/district-reports"
         element={
           <ProtectedRoute>
@@ -117,7 +146,53 @@ const AppRoutes = () => {
             <DistrictReports />
           </ProtectedRoute>
         }
-      />
+      >
+        <Route
+          path="overview"
+          element={
+            <>
+              {console.log('AppRoutes.jsx - Rendering DistrictOverview component')}
+              <DistrictOverview />
+            </>
+          }
+        />
+        <Route
+          path="school-reports"
+          element={
+            <>
+              {console.log('AppRoutes.jsx - Rendering SchoolReports component')}
+              <SchoolReports />
+            </>
+          }
+        />
+        <Route
+          path="student-reports"
+          element={
+            <>
+              {console.log('AppRoutes.jsx - Rendering StudentReports component')}
+              <StudentReports />
+            </>
+          }
+        />
+        <Route
+          path="teacher-reports"
+          element={
+            <>
+              {console.log('AppRoutes.jsx - Rendering TeacherReports component')}
+              <TeacherReports />
+            </>
+          }
+        />
+        <Route
+          path="compliance-reports"
+          element={
+            <>
+              {console.log('AppRoutes.jsx - Rendering ComplianceReports component')}
+              <ComplianceReports />
+            </>
+          }
+        />
+      </Route>
       <Route
         path="/moe/database-tools"
         element={
@@ -128,11 +203,101 @@ const AppRoutes = () => {
         }
       />
       <Route
+        path="/moe/ministry-employee-management"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering MinistryEmployeeManagement component')}
+            <MinistryEmployeeManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/school-admin-management"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering SchoolAdminManagement component')}
+            <SchoolAdminManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
         path="/moe/user-management"
         element={
           <ProtectedRoute>
             {console.log('AppRoutes.jsx - Rendering UserManagement component')}
             <UserManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/add-parent"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering AddParent component')}
+            <AddParent />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/parent-details/:parentId"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering ParentDetails component')}
+            <ParentDetails />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/parent-management"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering ParentManagement component')}
+            <ParentManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/reports"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering Reports component')}
+            <Reports />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/announcement-management"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering AnnouncementManagement component')}
+            <AnnouncementManagement />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/add-announcement"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering AddAnnouncement component')}
+            <div>AddAnnouncement component placeholder</div>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/announcement-details"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering AnnouncementDetails component')}
+            <div>AnnouncementDetails component placeholder</div>
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/moe/student-management"
+        element={
+          <ProtectedRoute>
+            {console.log('AppRoutes.jsx - Rendering StudentManagement component')}
+            <div>StudentManagement component placeholder</div>
           </ProtectedRoute>
         }
       />
