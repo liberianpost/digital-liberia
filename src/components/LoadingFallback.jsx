9318495669473b12e88620d11671a07ea38ccbfd
Liberian Post <info@liberianpost.com>
2025-08-16 14:54:37 +0200

    Update and rename LoadingSpinner.jsx to LoadingFallback.jsx

--- src/components/LoadingFallback.jsx
+++ src/components/LoadingFallback.jsx
@@ -1,8 +1,8 @@
-const LoadingSpinner = () => (
+const LoadingFallback = () => (
   <div className="flex flex-col items-center justify-center space-y-4">
     <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
     <p className="text-blue-100">Authenticating...</p>
   </div>
 );
 
-export default LoadingSpinner;
+export default LoadingFallback;
