9edf19ad6e0bee5a346c8aa13650805d6f415331
Liberian Post <info@liberianpost.com>
2025-08-17 10:13:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,21 @@ import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@context', replacement: path.resolve(__dirname, 'src/context') },
-      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
-      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
-      { find: '@config', replacement: path.resolve(__dirname, 'src/config') },
-      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') }
-    ],
-    extensions: ['.js', '.jsx']
-  },
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer]
+      plugins: [
+        tailwindcss(),
+        autoprefixer()
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
+    outDir: 'dist'
   },
   base: './'
 });
