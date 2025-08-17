dc1395c6ac0ed4d5fd17f47fd4501913115f26b3
Liberian Post <info@liberianpost.com>
2025-08-17 02:00:59 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,9 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
+    'postcss-combine-media-query': {},
+    'postcss-combine-duplicated-selectors': {
+      removeDuplicatedProperties: true
+    }
+  }
 };
